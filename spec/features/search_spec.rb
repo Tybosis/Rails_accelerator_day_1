71adf7b039018a9ba0823d059a582c04037fe55a
Tyler Pottle <tyler.pottle@gmail.com>
2015-03-03 16:49:52 -0800

    fix hash from rocket to symbol notation for style

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'google home page' do
   it 'should display content after search' do
     visit 'http://www.google.com'
-    fill_in 'q', :with => 'HBO'
+    fill_in 'q', with: 'HBO'
     click_button 'Google Search'
     page.text.must_include 'HBO GO'
   end
